531037ca58d608f48ebddef7abd287eb9f69d33c
Ingrid <mtra.ingrid.lopez@gmail.com>
2024-09-20 13:31:34 -0700

    Se agregó el proyecto, convertir de dlls a euros, HTML, CSS y JS.

---
+++ convertir-usd-a-eur/README.md
@@ -0,0 +1,4 @@
+# Convertir USD a EUR
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Ingrid-Lopez/pen/VwJoEzd](https://codepen.io/Ingrid-Lopez/pen/VwJoEzd).
+
---
+++ convertir-usd-a-eur/dist/script.js
@@ -0,0 +1,25 @@
+//Declaración de constantes.
+//Se declara la contante y se llama el valor del id para obtener los elementos del html.
+const form = document.querySelector('#form')                                      
+const result = document.querySelector('#result')
+const dollars = document.querySelector('#dollars')
+
+//Evento que previene el submit a otra página.
+form.addEventListener('submit', (e) => {
+  //Prevenir que haga submit con action
+  e.preventDefault();
+
+  //Number() convierte el string en valor numérico.
+  //La const eur es estática, valor definido.
+  const usd = dollars.value
+  const usdNumber = Number(usd)
+  const eur = 21.63
+
+  //cosnt para almacenar la operación con los parametros establecidos.
+  const converter = usdNumber * eur
+  
+  //Se refleja el resultado en la etiqueta <p>
+  //toFixed() delimita la cantidad de ceros a utilizar después del punto.
+  // se concatena el resultado con el string + " €" para imprimir el resultado con el valor euro.
+  result.textContent = converter.toFixed(2) + " €"
+});
